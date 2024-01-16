48c61ceec9cd1ea6ab297c50f47b6701c3078e64
kyj91032 <sees77770@gmail.com>
2024-01-16 17:59:48 +0900

    Row, Arrangement, Alignment

--- app/src/main/java/com/example/kotlin_compose_dev/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_dev/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,25 +34,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    // Surface는 Jetpack Compose에서 Material Design의 기본적인 배경을 제공하는 컴포저블. 컨테이너의 역할을 한다.
     Surface(
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Surface(
-            color = Color.Magenta,
-            modifier = Modifier.wrapContentSize(
-                align = Alignment.Center
-                // align 의 기본값은 Alignment.Center
-                // align은 자식 컴포저블이 부모 컴포저블 내에서 어떤 위치에 배치될지를 결정한다
-            ) // wrapContentSize는 자식 컴포저블의 크기에 맞게 Surface의 크기를 조정한다
+        // 여러 개의 자식을 가질 수 있는 Row, Column, Box 컴포저블
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            // horizontalArrangement: 가로(수평) 방향에서 자식 요소들의 배치를 조정
+            // 수평 방향은 배열이므로 Arrangement 라고 명명
+            // SpaceAround: 자식 요소들의 양쪽에 여백을 두고 일정 간격 배치
+            // SpaceBetween: 자식 요소들의 양쪽에 여백이 없이 일정 간격 배치
+            // SpaceEvenly: 자식 요소들을 여백과 같은 일정 간격으로 배치
+            horizontalArrangement = Arrangement.SpaceBetween,
+            // verticalAlignment: 세로(수직) 방향에서 자식 요소들의 배치를 조정
+            // 수직 방향에는 정렬만이 가능하므로 Alignment 라고 명명
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = "Wrapped Content", // Text는 Modifier.wrapContentSize() 를 명시할 필요가 없음
-                style = MaterialTheme.typography.headlineLarge
-            )
+            HorizontalColoredBar(Color.Red)
+            HorizontalColoredBar(Color.Magenta)
+            HorizontalColoredBar(Color.Cyan)
+            HorizontalColoredBar(Color.Yellow)
+            HorizontalColoredBar(Color.Blue)
         }
     }
+}
+
+// 사용자 정의 컴포저블을 이용해서 반복되는 코드를 줄일 수 있음
+// 변하는 부분을 파라미터로 받아서 사용
+@Composable
+fun HorizontalColoredBar(color: Color) {
+    Surface(
+        color = color,
+        modifier = Modifier
+            .height(600.dp)
+            .width(60.dp)
+    ) { }
 }
 
 @Preview(showBackground = true)
