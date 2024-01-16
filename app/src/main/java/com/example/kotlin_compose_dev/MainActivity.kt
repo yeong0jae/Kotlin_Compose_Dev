687886e2c9e84ab76a58b78c93b105fb2b6f1380
kyj91032 <sees77770@gmail.com>
2024-01-16 19:37:12 +0900

    Column

--- app/src/main/java/com/example/kotlin_compose_dev/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_dev/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -38,37 +39,33 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        // 여러 개의 자식을 가질 수 있는 Row, Column, Box 컴포저블
-        Row(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            // horizontalArrangement: 가로(수평) 방향에서 자식 요소들의 배치를 조정
-            // 수평 방향은 배열이므로 Arrangement 라고 명명
-            // SpaceAround: 자식 요소들의 양쪽에 여백을 두고 일정 간격 배치
-            // SpaceBetween: 자식 요소들의 양쪽에 여백이 없이 일정 간격 배치
-            // SpaceEvenly: 자식 요소들을 여백과 같은 일정 간격으로 배치
-            horizontalArrangement = Arrangement.SpaceBetween,
-            // verticalAlignment: 세로(수직) 방향에서 자식 요소들의 배치를 조정
-            // 수직 방향에는 정렬만이 가능하므로 Alignment 라고 명명
-            verticalAlignment = Alignment.CenterVertically
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HorizontalColoredBar(Color.Red)
-            HorizontalColoredBar(Color.Magenta)
-            HorizontalColoredBar(Color.Cyan)
-            HorizontalColoredBar(Color.Yellow)
-            HorizontalColoredBar(Color.Blue)
+            Row(
+                // fillMaxWidth()는 Row의 width를 상위 컴포저블의 width만큼 채우도록 함
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                ColoredSquare(Color.Red)
+                ColoredSquare(Color.Magenta)
+            }
+            ColoredSquare(Color.Cyan)
+            ColoredSquare(Color.Yellow)
+            ColoredSquare(Color.Blue)
         }
     }
 }
 
-// 사용자 정의 컴포저블을 이용해서 반복되는 코드를 줄일 수 있음
-// 변하는 부분을 파라미터로 받아서 사용
 @Composable
-fun HorizontalColoredBar(color: Color) {
+fun ColoredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
-            .height(600.dp)
-            .width(60.dp)
+            .height(100.dp)
+            .width(100.dp)
     ) { }
 }
 
